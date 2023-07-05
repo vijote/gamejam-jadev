d8d37400d311babe2f8d6ea55f8901225ee4bc5b
Juan Dominguez <juanignaciodom3@gmail.com>
2023-07-05 01:45:48 -0300

    fix pause menu logic error

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -19,6 +19,8 @@ public class PauseMenu : MonoBehaviour
 
     private void Update()
     {
+        if (GameStateManager.state == GameState.Over) return;
+
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             OnResumeButtonClick();
